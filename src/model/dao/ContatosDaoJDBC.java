6b9959aafeabd50576c0f27feb623005a8dffd0e
55DevDanilo <dev.danilo_asl@outlook.com>
2026-04-13 22:57:41 -0300

    Finalizando a implementação do metodo findByName

--- src/model/dao/ContatosDaoJDBC.java
+++ src/model/dao/ContatosDaoJDBC.java
@@ -10,6 +10,7 @@ import java.util.List;
 import db.DB;
 import db.DbException;
 import entities.Contatos;
+import module.entities.Department;
 
 public class ContatosDaoJDBC implements ContatosDao {
 
@@ -24,8 +25,7 @@ public class ContatosDaoJDBC implements ContatosDao {
 		// TODO Auto-generated method stub
 		PreparedStatement st = null;
 		try {
-			st = conn.prepareStatement(
-					"INSERT INTO contatos " + " (Nome, Telefone, Email) " + "VALUES " + "(?, ?, ?)",
+			st = conn.prepareStatement("INSERT INTO contatos " + " (Nome, Telefone, Email) " + "VALUES " + "(?, ?, ?)",
 					Statement.RETURN_GENERATED_KEYS);
 			st.setString(1, c.getNome());
 			st.setString(2, c.getTelefone());
@@ -99,24 +99,57 @@ public class ContatosDaoJDBC implements ContatosDao {
 	@Override
 	public Contatos findByName(String nome) {
 		// TODO Auto-generated method stub
-		PreparedStatement st =  null;
+		PreparedStatement st = null;
 		ResultSet rs = null;
 		try {
-			
-			st=conn.prepareStatement("SELECT contatos")
-			
-		} catch (Exception e) {
-			// TODO: handle exception
+
+			st = conn.prepareStatement("SELECT * FROM contatos WHERE nome = ?");
+			st.setString(1, nome);
+			rs = st.executeQuery();
+			if (rs.next()) {
+				
+				return instantContatos(rs);
+			}
+			return null;
+
+		} catch (
+
+		SQLException e) {
+			throw new DbException(e.getMessage());
+
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
 		}
-		
-		return null;
+	}
+
+	private Contatos instantContatos(ResultSet rs) throws SQLException {
+		Contatos cont = new Contatos();
+		cont.setNome(rs.getString("nome"));
+		cont.setEmail(rs.getString("email"));
+		cont.setTelefone(rs.getString("telefone"));
+		return cont;
 	}
 
 	@Override
 	public Contatos findById(Integer id) {
 		// TODO Auto-generated method stub
-		return null;
-	}
+		{
+			// TODO Auto-generated method stub
+			PreparedStatement st =  null;
+			ResultSet rs = null;
+			try {
+				
+				st=conn.prepareStatement("SELECT * FROM contatos WHERE id = ?");
+				st.setInt(1, id);
+				
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+			
+			return null;
+		}
 
 	@Override
 	public List<Contatos> findAll() {
